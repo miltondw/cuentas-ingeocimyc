4e0b1855f3490f8e26d316b198682ba1ac24c7c5
miltondw <estradamilton2001@gmail.com>
2025-02-19 20:30:12 -0500

    fix routes

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,16 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+   base: '/',
   server: {
+    open: true,
+  },
+/*  server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
     },
     host: 'localhost', // Asegúrate de que el servidor escuche en localhost
     port: 5173,        // Asegúrate de que el puerto sea 5173
-  },
+  },*/
 });
