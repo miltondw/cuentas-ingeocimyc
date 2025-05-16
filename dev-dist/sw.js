42f6ce4627dc420047c3c0dde22a9445d02f6c62
miltondw <estradamilton2001@gmail.com>
2025-05-15 22:52:46 -0500

    fix: Update revision for index.html in service worker precache

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f89d6064'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n13e1ko64i8"
+    "revision": "0.b92ebd2ljq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
